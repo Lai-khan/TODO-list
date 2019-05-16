dd74e92f46d97a28a092a60bd6fbf5b65699db57
Lai-khan <seogong9026@gmail.com>
2019-05-16 15:28:48 +0900

    +추가

--- script_create.js
+++ script_create.js
@@ -5,26 +5,18 @@ $(".addbtn").click(function() {
     alert("Write Something!");
   }
   else {
+    // 입력한 내용 리스트에 추가 &
+    // 항목 li 옆에 내용 삭제 버튼 붙이기
     var txt = "<li>"+value+"<span>×</span></li>";
     $("ul").append(txt);
     $("span").addClass("close");
   }
   document.getElementById("inputvalue").value = "";
 
+  // 삭제 버튼 누르면 해당 항목 삭제
   $(".close").click(function() {
     $(this).parent("li").remove();
   });
-});
-
-// 항목 li 옆에 내용 삭제 버튼 붙이기
-$("li").each( function() {
-  $(this).append("<span>×</span>");
-  $("span").addClass("close");
-});
-
-// 삭제 버튼 누르면 해당 항목 삭제
-$(".close").click(function() {
-  $(this).parent("li").remove();
 });
 
 // 체크박스를 체크하면 마감기한이 나타난다.
