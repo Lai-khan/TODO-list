b4abc73dc88818445471ed0b046a21aee6cddd1e
Lai-khan <seogong9026@gmail.com>
2019-05-16 15:26:31 +0900

    create.html 수정

--- script_create.js
+++ script_create.js
@@ -1,11 +1,35 @@
 // 항목 추가 버튼
-$("#add_btn").click(function() {
-  $("ul").append('<li><input type="text" placeholder="Write down what to do."></li>');
+$(".addbtn").click(function() {
+  var value = document.getElementById("inputvalue").value;
+  if(value == '') {
+    alert("Write Something!");
+  }
+  else {
+    var txt = "<li>"+value+"<span>×</span></li>";
+    $("ul").append(txt);
+    $("span").addClass("close");
+  }
+  document.getElementById("inputvalue").value = "";
+
+  $(".close").click(function() {
+    $(this).parent("li").remove();
+  });
+});
+
+// 항목 li 옆에 내용 삭제 버튼 붙이기
+$("li").each( function() {
+  $(this).append("<span>×</span>");
+  $("span").addClass("close");
+});
+
+// 삭제 버튼 누르면 해당 항목 삭제
+$(".close").click(function() {
+  $(this).parent("li").remove();
 });
 
 // 체크박스를 체크하면 마감기한이 나타난다.
-$('input[type="checkbox"]').click(function() {
-  if ($('input:checkbox[name="deadline"]').is(":checked") == true) {
+$(".container").click(function() {
+  if ($('input:checkbox').is(":checked") == true) {
     $('input[type="date"]').css("visibility", "visible");
   }
   else {
