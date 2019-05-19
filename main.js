7eb2fc3213c7136579411b4b645542cf0d399cc5
Lai-khan <40736969+Lai-khan@users.noreply.github.com>
2019-05-19 15:42:10 +0900

    Security

--- main.js
+++ main.js
@@ -32,7 +32,7 @@ app.get('/page/:pageId', function(req, res, next) {
       <div class="footer">
         <a href="/update/${title}">수정</a>
         <form action="/delete_process" method="post">
-          <input type="hidden" name="id" value="${title}">
+          <input type="hidden" name="id" value="${sanitizeHtml(title)}">
           <input type="submit" value="삭제" onclick='btn_click("delete")'>
         </form>
       </div>
@@ -189,8 +189,8 @@ app.get('/update/:pageId', function(req, res, next) {
               <div class="header">
                 <h1>TODO List</h1>
                 <h3>Title</h3>
-                <input name="old_id" type="hidden" value=${result[0].id}>
-                <input name="title" id="title" type="text" value=${title}>
+                <input name="old_id" type="hidden" value=${sanitizeHtml(result[0].id)}>
+                <input name="title" id="title" type="text" value=${sanitizeHtml(title)}>
               </div>
               <div class="addlist">
                 <h3>Add list</h3>
@@ -210,7 +210,7 @@ app.get('/update/:pageId', function(req, res, next) {
                   <input type="checkbox">
                   <div class="checkmark"></div>
                 </label>
-                <input type="date" id="due" name="dueDate" value=${result[0].deadline}>
+                <input type="date" id="due" name="dueDate" value=${sanitizeHtml(result[0].deadline)}>
                 <div class="footer">
                   <input type="submit" value="완료">
                 </div>
@@ -316,107 +316,6 @@ app.post('/update_process', function(req, res, next) {
       }
     });
   }
-
-  /*
-  if(deadline == 1) {
-    // 마감기한이 설정 O
-    console.log(1);
-    db.query(`UPDATE todolist SET title=?, checked=1, deadline=? WHERE todolist.title=?`, 
-    [title, dueDate, old_title], function(error, result) {
-      if(error) next(error);
-      else {
-        console.log(2);
-        db.query(`DELETE FROM task WHERE task.todo_id=?`, [old_id], 
-        function(error1, result2) {
-          if(error1) next(error1);
-          else {
-            console.log(3);
-            if(check.length == 1) { // 항목이 1개면
-              if(check[i] == 1) { // if list checked
-                db.query(`INSERT INTO task (todo_id, task, checked) 
-                VALUES (?, ?, 1)`, [result.insertId, list], function(error2, result2) {
-                  if(error2) next(error2);
-                  console.log(4);
-                });
-              } else {
-                db.query(`INSERT INTO task (todo_id, task, checked) 
-                VALUES (?, ?, 0)`, [result.insertId, list], function(error2, result2) {
-                  if(error2) next(error2);
-                  console.log(5);
-                });
-              }
-            }
-            else {
-              console.log(6);
-              var i=0;
-              while(i < list.length) {
-                if(check[i] == 1) { // if list checked
-                  db.query(`INSERT INTO task (todo_id, task, checked) 
-                  VALUES (?, ?, 1)`, [result.insertId, list[i]], function(error2, result2) {
-                    if(error2) next(error2);
-                    console.log(7);
-                  });
-                } else {
-                  db.query(`INSERT INTO task (todo_id, task, checked) 
-                  VALUES (?, ?, 0)`, [result.insertId, list[i]], function(error2, result2) {
-                    if(error2) next(error2);
-                    console.log(8);
-                  });
-                }
-                i = i+1;
-              }
-            }
-            res.redirect(`/page/${title}`);
-          }
-        });
-      }
-    });
-  } else {
-    // 마감기한 체크 X
-    db.query(`UPDATE todolist SET title=?, checked=0, WHERE todolist.title=?`, 
-    [title, old_title], function(error, result) {
-      if(error) next(error);
-      else {
-        db.query(`DELETE FROM task WHERE task.todo_id=?`, [old_id], 
-        function(error1, result2) {
-          if(error1) next(error1)
-          else {
-            if(check.length == 1) { // 항목이 1개면
-              if(check[i] == 1) { // if list checked
-                db.query(`INSERT INTO task (todo_id, task, checked) 
-                VALUES (?, ?, 1)`, [result.insertId, list], function(error2, result2) {
-                  if(error2) next(error2);
-                });
-              } else {
-                db.query(`INSERT INTO task (todo_id, task, checked) 
-                VALUES (?, ?, 0)`, [result.insertId, list], function(error2, result2) {
-                  if(error2) next(error2);
-                });
-              }
-            }
-            else {
-              var i=0;
-              while(i < list.length) {
-                if(check[i] == 1) { // if list checked
-                  db.query(`INSERT INTO task (todo_id, task, checked) 
-                  VALUES (?, ?, 1)`, [result.insertId, list[i]], function(error2, result2) {
-                    if(error2) next(error2);
-                  });
-                } else {
-                  db.query(`INSERT INTO task (todo_id, task, checked) 
-                  VALUES (?, ?, 0)`, [result.insertId, list[i]], function(error2, result2) {
-                    if(error2) next(error2);
-                  });
-                }
-                i = i+1;
-              }
-            }
-            res.redirect(`/page/${title}`);
-          }
-        });
-      }
-    });
-  }*/
 });
 
 app.post('/checked_process', function(req, res, next) {
@@ -480,178 +379,3 @@ app.use(function (err, req, res, next) {
 app.listen(3000, function() {
   console.log('Example app listening on port 3000!')
 });
-
-
-
-
-
-
-
-
-
-/*
-var http = require('http');
-var fs = require('fs');
-var url = require('url');
-var qs = require('querystring');
-var template = require('./lib/template.js');
-
-var app = http.createServer(function(request,response){
-  var _url = request.url;
-  var queryData = url.parse(_url, true).query;
-  var pathname = url.parse(_url, true).pathname;
-
-  if(pathname === '/') {
-    if(queryData.id === undefined) {
-      // index 페이지
-      fs.readdir('./data', function(error, fileList) {
-        var title = 'Welcome';
-        var header = `
-        <div id="header">
-          <div id="h-1">
-            <h1><a href="/">TODO LIST</a></h1>
-            <center>
-              <p>Welcome to my TODO List Website</p>
-              <a href="/create">Create New</a>
-            </center>
-          </div>
-        </div>
-        `;
-        var list = template.List(fileList);
-        var html = template.htmlIndex(title, header, '', list);
-        response.writeHead(200);
-        response.end(html);
-      });
-    } else {
-      // view 페이지
-      fs.readFile(`data/${queryData.id}`, 'utf8', function(err, list) {
-        var title = queryData.id;
-        var header = `
-        <div id="header">
-          <div id="h-1">
-            <h1><a href="/">TODO LIST</a></h1>
-            <div id="center"><a href="/create">Create New</a></div>
-          </div>
-        </div>
-        `;
-        var footer = `
-        <div class="footer">
-          <form action="checked_process" method="post">
-            <input type="hidden" name="id" value="${list}">
-            <input type="submit" value="저장">
-          </form>
-          <a href="/update">수정</a>
-          <form action="delete_process" method="post">
-            <input type="hidden" name="id" value="${title}">
-            <input type="submit" value="삭제">
-          </form>
-        </div>
-        `
-        var array = list.toString().split(',');
-        var list = template.View(title, array);
-        var html = template.htmlView(title, header, footer, list);
-        response.writeHead(200);
-        response.end(html);
-      });
-    }
-  } else if(pathname === '/create') {
-    // TODO 새로 만들 때
-    var title = 'Create';
-    var header = `
-    <div id="header">
-      <div id="h-1">
-        <h1><a href="/">TODO LIST</a></h1>
-        <div id="center"><a href="/create">Create New</a></div>
-      </div>
-    </div>
-    <div id="article">
-      <form name="create" action="create_process" method="post">
-        <div class="header">
-          <h1>TODO List</h1>
-          <h3>Title</h3>
-          <input name="title" id="title" type="text">
-        </div>
-        <div class="addlist">
-          <h3>Add list</h3>
-          <input type="text" id="addlist">
-          <button class="addbtn">Add</button>
-          <p class="help">입력하고 Add를 누르면 추가됩니다.</p>
-        </div>
-        <h3>TODO list</h3>
-        <ul class="todo-list">
-        `
-    var body = `
-        <li>
-          <label>Study</label>
-          <input name="list" type="text">
-          <button class="edit">Edit</button>
-          <button class="delete">Delete</button>
-        </li>
-        <li class="checked">
-          <label>Book</label>
-          <input name="list" type="text">
-          <button class="edit">Edit</button>
-          <button class="delete">Delete</button>
-        </li>
-        `;
-    var footer = `
-        </ul>
-        <label class="container">선택 시 마감기한을 설정할 수 있습니다.
-          <input name="deadline" type="checkbox">
-          <div class="checkmark"></div>
-        </label>
-        <input type="date" name="dueDate">
-        <div class="footer">
-          <input type="submit" value="완료">
-        </div>
-      </form>
-    </div>
-    `;
-    var html = template.htmlCU(title, header, footer, body);
-    response.writeHead(200);
-    response.end(html);
-  } else if (pathname === '/create_process') {
-    // create 처리
-    var body = '';
-    request.on('data', function(data){
-        body = body + data;
-    });
-    request.on('end', function(){
-      var post = qs.parse(body);
-      var title = post.title;
-      var list = post.list;
-      var deadline = post.deadline;
-      var dueDate = post.date;
-      fs.writeFile(`data/${title}`, list, function(err){
-        response.writeHead(302, {Location: `/?id=${title}`});
-        response.end();
-      });
-    });
-  // } else if (pathname === '/update') {
-  //   // TODO 항목(제목, 내용) 수정할 때
-  // } else if (pathname === '/update_process') {
-  //   // update 처리
-  // } else if (pathname === '/checked_process') {
-  //   // 할 일 완료 처리
-  } else if (pathname === '/delete_process') {
-    // delete 처리
-    var body = '';
-    request.on('data', function(data){
-          body = body + data;
-    });
-    request.on('end', function(){
-      var post = qs.parse(body);
-      var id = post.id;
-      fs.unlink(`data/${id}`, function(error){
-        response.writeHead(302, {Location: `/`});
-        response.end();
-      })
-    });
-  } else {
-    response.writeHead(404);
-    response.end('Not found');
-  }
-
-});
-app.listen(3000);
-*/
